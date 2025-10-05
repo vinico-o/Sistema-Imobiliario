a7c9f5ac132556d78bfc144186a0d06969ce6591
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-10-05 13:36:17 -0300

    feat: add CasaResidencial class

---
+++ SistemaImobiliario/src/CasaResidencial.java
@@ -0,0 +1,22 @@
+
+import java.time.LocalDate;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Vinícius Mardegan
+ */
+public class CasaResidencial extends Imovel {
+    //CONSTRUTORES
+    public CasaResidencial() {
+        
+    }
+    
+    public CasaResidencial(int codigoImovel, String endereco, LocalDate dataConstrucao, float areaTotal, float areaConstruida, int qtdDormitorios, int qtdBanheiros, int qtdsVagasGaragem, float valorIPTU, float valorVenda, float valorAluguel) {
+        super(codigoImovel, endereco, dataConstrucao, areaTotal, areaConstruida, qtdDormitorios, qtdBanheiros, qtdsVagasGaragem, valorIPTU, valorVenda, valorAluguel);
+    }
+}

84356eef359bd37be6873e61a5d09b6b50511be9
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-10-05 13:58:18 -0300

    feat: add Seguro class

---
+++ SistemaImobiliario/src/seguro/Seguro.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package seguro;
+
+/**
+ *
+ * @author Vinícius Mardegan
+ */
+public class Seguro {
+    private int codigoSEguro;
+    private String nomeSeguradora;
+    private String tipo;
+    private String descricao;
+    private float valor;
+    
+    //CONSTRUTORES
+    public Seguro() {
+        
+    }
+
+    public Seguro(int codigoSEguro, String nomeSeguradora, String tipo, String descricao, float valor) {
+        this.codigoSEguro = codigoSEguro;
+        this.nomeSeguradora = nomeSeguradora;
+        this.tipo = tipo;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+    
+    //METODOS
+    public void imprimir() {
+        System.out.println("DADOS DO SEGURO");
+        System.out.println("Código do Seguro: " + codigoSEguro);
+        System.out.println("Nome da Seguradora: " + nomeSeguradora);
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Valor: R$ " + valor);
+    }
+    
+    //GETTERS E SETTERS
+    public int getCodigoSEguro() {
+        return codigoSEguro;
+    }
+
+    public void setCodigoSEguro(int codigoSEguro) {
+        this.codigoSEguro = codigoSEguro;
+    }
+
+    public String getNomeSeguradora() {
+        return nomeSeguradora;
+    }
+
+    public void setNomeSeguradora(String nomeSeguradora) {
+        this.nomeSeguradora = nomeSeguradora;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    
+}

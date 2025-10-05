1fd29129738b4d92394220c5fcb072466e7cd9a0
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-10-05 13:37:27 -0300

    feat: add Comercial class

---
+++ SistemaImobiliario/src/Comercial.java
@@ -0,0 +1,47 @@
+
+import java.time.LocalDate;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Vinícius Mardegan
+ */
+public class Comercial extends Imovel {
+    protected float taxaImpostoFederal;
+    
+    //CONSTRUTORES
+    public Comercial() {
+        
+    }
+    
+    public Comercial(float taxaImpostoFederal, int codigoImovel, String endereco, LocalDate dataConstrucao, float areaTotal, float areaConstruida, int qtdDormitorios, int qtdBanheiros, int qtdsVagasGaragem, float valorIPTU, float valorVenda, float valorAluguel) {
+        super(codigoImovel, endereco, dataConstrucao, areaTotal, areaConstruida, qtdDormitorios, qtdBanheiros, qtdsVagasGaragem, valorIPTU, valorVenda, valorAluguel);
+        this.taxaImpostoFederal = taxaImpostoFederal;
+    }
+    
+    //METODOS
+    @Override
+    public void imprimir() {
+        super.imprimir();
+        System.out.println("Taxa de Imposto Federal: " + taxaImpostoFederal);
+    }
+    
+    @Override
+    public float getValorAluguel() {
+        return super.getValorAluguel() + super.getValorAluguel() * taxaImpostoFederal;
+    }
+    
+    //GETTERS E SETTERS
+    public float getTaxaImpostoFederal() {
+        return taxaImpostoFederal;
+    }
+
+    public void setTaxaImpostoFederal(float taxaImpostoFederal) {
+        this.taxaImpostoFederal = taxaImpostoFederal;
+    }
+    
+}

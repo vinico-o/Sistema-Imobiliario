3d0c6bde7dd011fef7c54fb6fd96069e9385f6df
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-10-05 23:23:57 -0300

    docs: rename package to pagamento in .java

--- SistemaImobiliario/src/pagamento/Pagamento_CauanIgorVinicius.java
+++ SistemaImobiliario/src/pagamento/Pagamento_CauanIgorVinicius.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package trabalho;
+package pagamento;
 
 /**
  *
@@ -28,5 +28,5 @@ public abstract class Pagamento_CauanIgorVinicius {
     
     
     @Override
-    public abstract String toString()
+    public abstract String toString();
 }

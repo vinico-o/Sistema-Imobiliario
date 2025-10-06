39cd66ac66dc30a18cf95a7e0ee95ace8deabe66
RomeroNog <igorromeronigueira@gmail.com>
2025-10-05 23:18:54 -0300

    feat: pagamento package

---
+++ SistemaImobiliario/src/pagamento/Pagamento_CauanIgorVinicius.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabalho;
+
+/**
+ *
+ * @author manoe
+ */
+public abstract class Pagamento_CauanIgorVinicius {
+    private String tipoPagamento;
+
+    public Pagamento_CauanIgorVinicius() {}
+
+    public Pagamento_CauanIgorVinicius(String tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
+    }
+
+    public String getTipoPagamento() {
+        return tipoPagamento;
+    }
+
+    public void setTipoPagamento(String tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
+    }
+    
+    
+    
+    @Override
+    public abstract String toString()
+}

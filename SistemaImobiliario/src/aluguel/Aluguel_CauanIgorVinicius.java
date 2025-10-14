7f463526e7a53e5c465880cf3b31bcaf7368b1ef
RomeroNog <igorromeronigueira@gmail.com>
2025-10-14 11:43:01 -0300

    feat: implementação parcial das classes Aluguel e Venda

--- SistemaImobiliario/src/aluguel/Aluguel_CauanIgorVinicius.java
+++ SistemaImobiliario/src/aluguel/Aluguel_CauanIgorVinicius.java
@@ -4,10 +4,201 @@
  */
 package aluguel;
 
+import imovel.Imovel_CauanIgorVinicius;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import pagamento.Pagamento_CauanIgorVinicius;
+import seguro.Seguro_CauanIgorVinicius;
+import usuario.Cliente_CauanIgorVinicius;
+import usuario.Corretor_CauanIgorVinicius;
+
 /**
  *
  * @author manoe
  */
 public class Aluguel_CauanIgorVinicius {
+    private int codigoAluguel;
+    private Cliente_CauanIgorVinicius cliente;
+    private Corretor_CauanIgorVinicius corretor;
+    private Imovel_CauanIgorVinicius imovel;
+    private LocalDate dataAluguel;
+    private LocalDate dataDevolucao;
+    private LocalDate dataPagamentoMensal;
+    private float valorTotalAluguel;
+    private Pagamento_CauanIgorVinicius formaPagamento;
+    private ArrayList<Seguro_CauanIgorVinicius> segurosContratados = new ArrayList<>();
+    private Boolean finalizado;
+    private Boolean pago;
+
+    
+    public Aluguel_CauanIgorVinicius(){}
+
+    public Aluguel_CauanIgorVinicius(int codigoAluguel, Cliente_CauanIgorVinicius cliente, Corretor_CauanIgorVinicius corretor, Imovel_CauanIgorVinicius imovel, LocalDate dataAluguel, LocalDate dataDevolucao, LocalDate dataPagamentoMensal, Pagamento_CauanIgorVinicius formaPagamento, ArrayList<Seguro_CauanIgorVinicius> segurosContratados, Boolean finalizado, Boolean pago) {
+        this.codigoAluguel = codigoAluguel;
+        this.cliente = cliente;
+        this.corretor = corretor;
+        this.imovel = imovel;
+        this.dataAluguel = dataAluguel;
+        this.dataDevolucao = dataDevolucao;
+        this.dataPagamentoMensal = dataPagamentoMensal;
+        this.formaPagamento = formaPagamento;
+        this.segurosContratados = segurosContratados;
+        this.finalizado = finalizado;
+        this.pago = pago;
+        this.valorTotalAluguel = this.calcularValorTotal();
+    }
+
+    public int getCodigoAluguel() {
+        return codigoAluguel;
+    }
+
+    public void setCodigoAluguel(int codigoAluguel) {
+        this.codigoAluguel = codigoAluguel;
+    }
+
+    public Cliente_CauanIgorVinicius getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente_CauanIgorVinicius cliente) {
+        this.cliente = cliente;
+    }
+
+    public Corretor_CauanIgorVinicius getCorretor() {
+        return corretor;
+    }
+
+    public void setCorretor(Corretor_CauanIgorVinicius corretor) {
+        this.corretor = corretor;
+    }
+
+    public Imovel_CauanIgorVinicius getImovel() {
+        return imovel;
+    }
+
+    public void setImovel(Imovel_CauanIgorVinicius imovel) {
+        this.imovel = imovel;
+    }
+
+    public LocalDate getDataAluguel() {
+        return dataAluguel;
+    }
+
+    public void setDataAluguel(LocalDate dataAluguel) {
+        this.dataAluguel = dataAluguel;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(LocalDate dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public LocalDate getDataPagamentoMensal() {
+        return dataPagamentoMensal;
+    }
+
+    public void setDataPagamentoMensal(LocalDate dataPagamentoMensal) {
+        this.dataPagamentoMensal = dataPagamentoMensal;
+    }
+
+    public float getValorTotalAluguel() {
+        return valorTotalAluguel;
+    }
+
+    public void setValorTotalAluguel(float valorTotalAluguel) {
+        this.valorTotalAluguel = valorTotalAluguel;
+    }
+
+    public Pagamento_CauanIgorVinicius getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(Pagamento_CauanIgorVinicius formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public ArrayList<Seguro_CauanIgorVinicius> getSegurosContratados() {
+        return segurosContratados;
+    }
+
+    public void setSegurosContratados(ArrayList<Seguro_CauanIgorVinicius> segurosContratados) {
+        this.segurosContratados = segurosContratados;
+    }
+
+    public Boolean getFinalizado() {
+        return finalizado;
+    }
+
+    public void setFinalizado(Boolean finalizado) {
+        this.finalizado = finalizado;
+    }
+
+    public Boolean getPago() {
+        return pago;
+    }
+
+    public void setPago(Boolean pago) {
+        this.pago = pago;
+    }
+    
+    //Valores incluidos: valor aluguel(de imovel) e os valores de cada seguro
+    public float calcularValorTotal()
+    {
+        float custoTotal=0;
+        custoTotal+=imovel.getValorAluguel();
+        for(Seguro_CauanIgorVinicius s: segurosContratados)
+        {
+            custoTotal+=s.getValor();
+        }
+        return custoTotal;
+    }
+    
+    public Boolean possuiSeguro()
+    {
+       if(segurosContratados.isEmpty())
+       {
+           return false;
+       }
+       else
+       {
+           return true;
+       }
+    }
+    
+    public Boolean verificarAtraso()
+    {
+       if(this.pago == false)
+       {
+          if(LocalDate.now().isAfter(dataPagamentoMensal))
+          {
+              return true;
+          }
+          else 
+          {
+              return false;
+          }
+       }
+       return false;
+    }
+    
+    public String toString()
+    {
+       return "Aluguel{"+ "codigoAluguel=" + this.codigoAluguel + "\n"
+                        + "cliente=" + this.cliente + "\n"
+                        + "corretor=" + this.corretor + "\n"    
+                        + "imovel=" + this.imovel + "\n"
+                        + "dataAluguel=" + this.dataAluguel + "\n"
+                        + "dataDevolucao=" + this.dataDevolucao + "\n"
+                        + "dataPagamentoMensal=" + this.dataPagamentoMensal + "\n"
+                        + "valorTotalAluguel=" + this.valorTotalAluguel + "\n"
+                        + "formaPagamento=" + this.formaPagamento + "\n"
+                        + "segurosContratados=" + this.segurosContratados + "\n"
+                        + "finalizado=" + this.finalizado + "\n"
+                        + "pago=" + this.pago + "\n";        
+    }
+    
     
 }
--- SistemaImobiliario/src/venda/Venda_CauanIgorVinicius.java
+++ SistemaImobiliario/src/venda/Venda_CauanIgorVinicius.java
@@ -4,10 +4,103 @@
  */
 package venda;
 
+import imovel.Imovel_CauanIgorVinicius;
+import java.time.LocalDate;
+import usuario.Cliente_CauanIgorVinicius;
+import usuario.Corretor_CauanIgorVinicius;
+
 /**
  *
  * @author manoe
  */
 public class Venda_CauanIgorVinicius {
+    private int codigoVenda;
+    private Cliente_CauanIgorVinicius cliente;
+    private Corretor_CauanIgorVinicius corretor;
+    private Imovel_CauanIgorVinicius imovel;
+    private LocalDate dataVenda;
+    private float formaPagamento;
+    private Boolean finalizada;
+
+    
+    public Venda_CauanIgorVinicius() {}
+
+    public Venda_CauanIgorVinicius(int codigoVenda, Cliente_CauanIgorVinicius cliente, Corretor_CauanIgorVinicius corretor, Imovel_CauanIgorVinicius imovel, LocalDate dataVenda, float formaPagamento, Boolean finalizada) {
+        this.codigoVenda = codigoVenda;
+        this.cliente = cliente;
+        this.corretor = corretor;
+        this.imovel = imovel;
+        this.dataVenda = dataVenda;
+        this.formaPagamento = formaPagamento;
+        this.finalizada = finalizada;
+    }
+
+    public int getCodigoVenda() {
+        return codigoVenda;
+    }
+
+    public void setCodigoVenda(int codigoVenda) {
+        this.codigoVenda = codigoVenda;
+    }
+
+    public Cliente_CauanIgorVinicius getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente_CauanIgorVinicius cliente) {
+        this.cliente = cliente;
+    }
+
+    public Corretor_CauanIgorVinicius getCorretor() {
+        return corretor;
+    }
+
+    public void setCorretor(Corretor_CauanIgorVinicius corretor) {
+        this.corretor = corretor;
+    }
+
+    public Imovel_CauanIgorVinicius getImovel() {
+        return imovel;
+    }
+
+    public void setImovel(Imovel_CauanIgorVinicius imovel) {
+        this.imovel = imovel;
+    }
+
+    public LocalDate getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(LocalDate dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public float getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(float formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public Boolean getFinalizada() {
+        return finalizada;
+    }
+
+    public void setFinalizada(Boolean finalizada) {
+        this.finalizada = finalizada;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "Venda{" + "codigoVenda=" + this.codigoVenda + "\n"
+                        + "cliente=" + this.cliente + "\n"
+                        + "corretor=" + this.corretor + "\n"
+                        + "imovel=" + this.imovel + "\n"
+                        + "dataVenda=" + this.dataVenda + "\n"
+                        + "formaPagamento=" + this.formaPagamento + "\n"
+                        + "finalizada=" + this.finalizada + "}";
+    }                   
     
 }

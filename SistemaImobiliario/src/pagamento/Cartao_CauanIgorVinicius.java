6f9c9e884b31cfcc289e18b7f8cd360aa3a916d3
RomeroNog <igorromeronigueira@gmail.com>
2025-10-17 20:20:27 -0300

    feat: metodos cartão

--- SistemaImobiliario/src/pagamento/Cartao_CauanIgorVinicius.java
+++ SistemaImobiliario/src/pagamento/Cartao_CauanIgorVinicius.java
@@ -8,6 +8,47 @@ package pagamento;
  *
  * @author manoe
  */
-public class Cartao_CauanIgorVinicius {
+public class Cartao_CauanIgorVinicius extends Pagamento_CauanIgorVinicius{
+    private String nome,bandeira,numero;
+
+    public Cartao_CauanIgorVinicius() {}
+
+    public Cartao_CauanIgorVinicius(String nome, String bandeira, String numero, String tipoPagamento) {
+        super(tipoPagamento);
+        this.nome = nome;
+        this.bandeira = bandeira;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getBandeira() {
+        return bandeira;
+    }
+
+    public void setBandeira(String bandeira) {
+        this.bandeira = bandeira;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return getTipoPagamento();
+    }
+    
     
 }
